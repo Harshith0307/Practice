7e6e70d403c8914330e40681f77dfedb3719f36e
Harshith0307 <51245050+Harshith0307@users.noreply.github.com>
2019-12-26 14:39:42 -0600

    finding minimum number in an array with for loop

--- Min_array.py
+++ Min_array.py
@@ -1,10 +1,24 @@
 #read a list of numbers and find the minimum  
+def min_array(elements):
+    min = elements[0]
+    for item in elements:
+        if min > item:
+         min = item
+    return min
+
 if __name__ == '__main__':
 
     # count = int(input("Please enter the number of numbers dumma "))
 
     elements = list(map(int, input("please enter a string of numbers seperated by spaces to find the minimum : ").strip().split()))
 
-    answer = min(elements)
+    answer = min_array(elements)
 
     print(answer)
+
+    built_in_answer = min(elements)
+
+    if built_in_answer == answer:
+        print("Validation success")
+    else:
+        print("Validation failure")
