6faea489f331e4284da38b6cf98759ac5e2e8d80
Harshith0307 <51245050+Harshith0307@users.noreply.github.com>
2019-12-26 14:30:02 -0600

    finding minimum in an array of integers

--- Min_array.py
+++ Min_array.py
@@ -1,23 +1,10 @@
 #read a list of numbers and find the minimum  
-
-def square(element):
-    return element**2
-    
-
-def square_array(elements):
-
-    for item in elements:
-        print(square(item))
-    
-
 if __name__ == '__main__':
 
     # count = int(input("Please enter the number of numbers dumma "))
 
-    elements = list(map(int, input("please enter a string of numbers seperated by spaces : ).strip().split()))
+    elements = list(map(int, input("please enter a string of numbers seperated by spaces to find the minimum : ").strip().split()))
 
-    square_array(elements)
+    answer = min(elements)
 
-    squared_num = list(map(square, elements))
-
-    print(squared_num)
+    print(answer)
