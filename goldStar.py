52104b6da91224e676ba17aba6761910ce3b1699
Harshith0307 <51245050+Harshith0307@users.noreply.github.com>
2020-03-13 18:35:00 -0500

    I got my gold star in python

---
+++ goldStar.py
@@ -0,0 +1,10 @@
+class harshith:
+    def __init__(self, age, height):
+        pass
+
+    def goldStar(self, name):
+        print(name, " got a gold star in python on hackerrank")
+
+
+harsh = harshith(2,2)
+harsh.goldStar(harshith)
